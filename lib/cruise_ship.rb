719eee0feb9367cfe6f4d1c93153672ac8115215
Bryce Moore <brycemooore@gmail.com>
2020-09-22 19:49:43 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,7 @@
 def select_winner(passengers)
   winner = ''
   passengers.each {|suite, name|
-    if suite == :suite && name.start_with?("A")
+    if suite == :suite_a && name.start_with?("A")
       winner = name
     end
   }
